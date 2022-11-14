437ffd39e14e64e90466207601b18a0b967b0ae7
scurenza <fedechiar@hotmail.it>
2022-11-14 17:34:15 +0100

    problemi v-if

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -3,6 +3,10 @@ export default {
     name: "FilmCard",
     props: {
         film: Object
+    }, data() {
+        return {
+            flags: ["en"]
+        }
     }
 }
 </script>
@@ -12,7 +16,9 @@ export default {
         <h3 class="title">Titolo: {{ film.title }}</h3>
         <h4 class="original.title">Titolo originale: {{ film.original_title }} </h4>
         <div class="flag">
-            <img class="flag" :src=' "../../public/" + film.original_language + ".png"' :alt='"flag " + film.original_language'>
+            <p>Lang:</p>
+                <img :v-if="this.flags.includes(film.original_language)" class="flag" :src=' "../../public/" + film.original_language + ".png"' :alt='"flag" + " " + film.original_language'>
+                <p :v-else="!this.flags.includes(film.original_language)">{{ film.original_language }}</p>
         </div>
         <p class="score">Voto: {{ film.vote_average }}</p>
     </div>
@@ -21,7 +27,16 @@ export default {
 <style lang="scss" scoped>
 .card{
     border: 2px solid black;
-    width: 100%;
+    width: 350px;
     min-height: 400px;
+
+    * {
+        margin: 1rem .2rem;
+    }
+    .flag {
+        width: 50px;
+        display: flex;
+        align-items: center;
+    }
 }
 </style>
